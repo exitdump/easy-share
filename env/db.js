637fe063e1fb556feb2ec3cbbccdf001026e8864
Exit Dump <77228152+exitdump@users.noreply.github.com>
2024-09-30 05:51:15 +0600

    Add files via upload

---
+++ env/db.js
@@ -0,0 +1,5 @@
+const db = {
+    direct_links: ['https://roomshikinginformal.com/fsigagrmeu?key=6825167c4d607891a5f4b11e415f013c', 'https://roomshikinginformal.com/eas7bdxa0q?key=1fecf5a75c428dc7f8a851c76be4d59e'],
+}
+
+module.exports = db
---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const path = require('path');
+const db = require('./env/db.js');
+const app = express();
+
+// Set the view engine to EJS
+app.set('view engine', 'ejs');
+
+// Set views directory
+app.set('views', path.join(__dirname, 'views'));
+
+// Static files (CSS, images, etc.)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Landing page route
+app.get('/:title?', (req, res) => {
+
+    const random_link = db.direct_links[Math.floor(Math.random() * db.direct_links.length)];
+
+    const base64Regex = /^(?:[A-Z0-9+/]{4})*(?:[A-Z0-9+/]{2}==|[A-Z0-9+/]{3}=)?$/i;
+    if (req.query.i){
+        if (!base64Regex.test(req.query.i)) {
+            image_url_encoded = req.query.i;
+       } else {
+            image_url_encoded = atob(decodeURIComponent(req.query.i));
+       }
+    }
+
+    if (req.query.d){
+        if (!base64Regex.test(req.query.d)) {
+            dest_url_encoded = req.query.d;
+       } else {
+            dest_url_encoded = atob(decodeURIComponent(req.query.d));
+       }
+    }
+
+    const data = {
+        direct_link: random_link, // Random link from db.js
+        title: req.params.title && req.params.title, // Title from route param: /:title
+        img_url: req.query.i && image_url_encoded, // Image URL from query string: ?i=
+        dest: req.query.d && dest_url_encoded, // Destination URL from query string: ?d=
+    }
+
+    // console.log(data)
+    res.render('index', { data });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
