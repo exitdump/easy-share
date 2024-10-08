696cee17f07f1684028f3a4f4e07fa9baef69ed2
exitdump <jahidhasan100101@gmail.com>
2024-10-09 02:12:12 +0600

    change major

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const db = require('./env/db.js');
+const db = require('../env/db.js');
 const app = express();
 
 // Set the view engine to EJS
